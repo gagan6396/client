0bb872089c34bfcae4e97bc33856f884cad60c76
rishabh gehlot <rishabh3x@gmail.com>
2025-01-19 16:22:22 +0530

    '@typescript-eslint/no-explicit-any': 'off', // Disable the rule for any       '@typescript-eslint/no-unused-vars': 'off', // Disable the rule for unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Extend the existing Next.js ESLint configurations
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // Disable the rule for any
+      '@typescript-eslint/no-unused-vars': 'off', // Disable the rule for unused vars
+    },
+  },
 ];
 
 export default eslintConfig;
